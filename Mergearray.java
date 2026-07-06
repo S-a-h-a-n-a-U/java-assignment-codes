4c031326b58451cde010b4d0acf58287cc81de1c
S-a-h-a-n-a-U <sahana.u@nextuple.com>
2026-07-06 11:45:02 +0530

    Update Mergearray.java

--- Mergearray.java
+++ Mergearray.java
@@ -1,33 +1,31 @@
-
-
 import java.util.Arrays;
-
-public class Mergearray {
-
-    public static void main(String[] args) {
-
-        int[] arr1 = {1,5,9,10,15,20};
-        int[] arr2 = {2,3,8,13};
-
-        for(int i = arr2.length - 1; i >= 0; i--) {
-
-            int last = arr1[arr1.length - 1];
-
-            int j;
-
-            for(j = arr1.length - 2; j >= 0 && arr1[j] > arr2[i]; j--) {
-
-                arr1[j + 1] = arr1[j];
+public class Mergearray{
+    public static void main(String[] args){
+        try{
+            int[] arr1={1,5,9,10,15,20};
+            int[] arr2={2,3,8,13};
+            for(int i=arr2.length-1;i>=0;i--){
+                int last=arr1[arr1.length-1];
+                int j;
+                for(j=arr1.length-2;j>=0&&arr1[j]>arr2[i];j--){
+                    arr1[j+1]=arr1[j];
+                }
+                if(j!=arr1.length-2||last>arr2[i]){
+                    arr1[j+1]=arr2[i];
+                    arr2[i]=last;
+                }
             }
-
-            if(j != arr1.length - 2 || last > arr2[i]) {
-
-                arr1[j + 1] = arr2[i];
-                arr2[i] = last;
-            }
+            System.out.println("array 1: "+Arrays.toString(arr1));
+            System.out.println("Array 2: "+Arrays.toString(arr2));
         }
-
-        System.out.println(Arrays.toString(arr1));
-        System.out.println(Arrays.toString(arr2));
+        catch(NullPointerException e){
+            System.out.println("error: Array is null");
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("error: Invalid array index");
+        }
+        catch(Exception e){
+            System.out.println("Error: "+e.getMessage());
+        }           
     }
-}
+}          
