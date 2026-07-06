acb52bde893e2a2edfe075a1e98a4fb88ed90288
S-a-h-a-n-a-U <sahana.u@nextuple.com>
2026-07-06 12:25:58 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,15 +1,26 @@
 import java.util.*;
-class add {
-    public static void main(String[] args) {
-        System.out.println("Enter numbers to be added");
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println("The sum of number are: " +(a + b));
-        System.out.println("The sum is: " +(double)(a + b));
-        System.out.println("The sum is: " +(float)(a + b));
-        System.out.println("Character value of a: " +(char)a);
-        System.out.println("String value of sum: " + String.valueOf(a + b));
-sc.close();
+class add{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        try{
+            System.out.println("Enter two numbers:");
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int sum=a+b;
+            System.out.println("sum of the numbers is: "+sum);
+            System.out.println("sum as double: "+(double)sum);
+            System.out.println("sum as float: "+(float)sum);
+            System.out.println("character value of a: "+(char)a);
+            System.out.println("string value of sum: "+String.valueOf(sum));
+        }
+        catch(InputMismatchException e){
+            System.out.println("enter only integer values.");
+        }
+        catch(Exception e){
+            System.out.println("Error: "+e.getMessage());
+        }
+        finally{
+            sc.close();
+        }
     }
 }
