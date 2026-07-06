93d44407ac6e5e5881ae1fd78b6c632aa2f20c6a
S-a-h-a-n-a-U <sahana.u@nextuple.com>
2026-07-06 11:19:42 +0530

    Binarysearch.java

--- Binarysearch.java
+++ Binarysearch.java
@@ -1,34 +1,32 @@
 public class Binarysearch {
     public static void main(String[] args) {
-
-        int[] arr = {2, 4, 6, 8, 10, 12, 14};
-        int key = 10;
-
-        int low = 0;
-        int high = arr.length - 1;
-        int found = -1;
-
-        while (low <= high) {
-
-            int mid = (low + high) / 2;
-
-            if (arr[mid] == key) {
-                found = mid;
-                break;
-            }
-            else if (arr[mid] < key) {
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
-            }
-        }
-
-        if (found == -1) {
-            System.out.println("Element not found");
-        }
-        else {
-            System.out.println("Element found at index " + found);
+        try {
+            int[] arr = {2, 4, 6, 8, 10, 12, 14};
+            int key = 10;
+            int low = 0;
+            int high = arr.length - 1;
+            int found = -1;
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                if (arr[mid] == key) {
+                    found = mid;
+                    break;
+                } else if (arr[mid] < key) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                    }}
+            if (found == -1) {
+                System.out.println("element not found");
+            } else {
+                System.out.println("element found at index " + found);
+            }}
+          catch (NullPointerException e) {
+                System.out.println("Error: Array is null.");}
+         catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: Array index is out of bounds.");}
+        catch (Exception e) {
+            System.out.println("unexpected error occurred: " + e.getMessage());
         }
     }
 }
