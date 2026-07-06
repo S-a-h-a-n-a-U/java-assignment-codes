11c2ad63275e95caecba2eaa43fc6f5f2d4c2fab
S-a-h-a-n-a-U <sahana.u@nextuple.com>
2026-07-06 12:36:25 +0530

    Update Repeatedletters.java
    Refactored code to include error handling and improved output message.

--- Repeatedletters.java
+++ Repeatedletters.java
@@ -1,20 +1,22 @@
-public class Repeatedletters {
-    public static void main(String[] args) {
-
-        String str = "Sahana";
-        int[] count = new int[256];
-
-        for (int i = 0; i < str.length(); i++) {
-            count[str.charAt(i)]++;
-        }
-
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-
-            if (count[ch] > 1) {
-                System.out.println(ch + " = " + count[ch]);
-                count[ch] = 0;
+public class Repeatedletters{
+    public static void main(String[] args){
+        try{
+            String str="Sahana";
+            int[] count=new int[256];
+            for(int i=0;i<str.length();i++){
+                count[str.charAt(i)]++;
             }
+            System.out.println("Repeated characters:");
+            for(int i=0;i<str.length();i++){
+                char ch=str.charAt(i);
+                if(count[ch]>1){
+                    System.out.println(ch+" = "+count[ch]);
+                    count[ch]=0;
+                }
+            }
+        }
+        catch(Exception e){
+            System.out.println("Error: "+e.getMessage());
         }
     }
 }
