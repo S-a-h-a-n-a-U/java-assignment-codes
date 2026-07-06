c5e273e4f25d9cfc86141d4cce329f6cd9449fde
S-a-h-a-n-a-U <sahana.u@nextuple.com>
2026-07-06 12:08:09 +0530

    Update checkonlyoddoreven.java

--- checkonlyoddoreven.java
+++ checkonlyoddoreven.java
@@ -1,24 +1,24 @@
-    import java.util.Scanner;
-public class checkonlyoddoreven {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the number of elements: ");
-        int n=sc.nextInt();
-        int even=0,odd=0;
-        System.out.println("Enter the elements:");
-        for (int i=0; i < n; i++) {
-            int ele=sc.nextInt();
-            if (ele%2==0)
-                even++;
-            else
-                odd++;
+import java.util.Arrays;
+public class Mergearray{
+    public static void main(String[] args){
+        try{
+            int[] arr1={1,5,9,10,15,20};
+            int[] arr2={2,3,8,13};
+            for(int i=arr2.length-1;i>=0;i--){
+                int last=arr1[arr1.length-1];
+                int j;
+                for(j=arr1.length-2;j>=0&&arr1[j]>arr2[i];j--){
+                    arr1[j+1]=arr1[j];
+                }
+                if(j!=arr1.length-2||last>arr2[i]){
+                    arr1[j+1]=arr2[i];
+                    arr2[i]=last;
+                }
+            }
+            System.out.println("array 1: "+Arrays.toString(arr1));
+            System.out.println("array 2: "+Arrays.toString(arr2));
+        }catch(Exception e){
+            System.out.println("exception: "+e);
         }
-        if (even==n)
-            System.out.println("Only even numbers");
-        else if (odd==n)
-            System.out.println("Only odd numbers");
-        else
-            System.out.println("Contains both odd and even numbers");
-        sc.close();
     }
 }
