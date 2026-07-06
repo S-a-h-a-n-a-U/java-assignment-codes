c9ee72bd841e10b8096a443fafc27830a385c22d
S-a-h-a-n-a-U <sahana.u@nextuple.com>
2026-07-06 12:57:20 +0530

    Update anagram.java

--- anagram.java
+++ anagram.java
@@ -1,20 +1,28 @@
 import java.util.*;
-public class anagram {
-    public static void main(String[] args) {
+public class anagram{
+    public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter 1st string");
-        String s1=sc.next();
- System.out.println("Enter 2nd string");
- String s2=sc.next();
- char[] a=s1.toCharArray();
- char[] b=s2.toCharArray();
- Arrays.sort(a);
- Arrays.sort(b);
- {if(Arrays.equals(a,b))
- System.out.println("Anagram");
- else
-  System.out.println(" Not Anagram");}
-sc.close();
+        try{
+            System.out.println("enter the first string:");
+            String s1=sc.next();
+            System.out.println("enter the second string:");
+            String s2=sc.next();
+            char[] a=s1.toLowerCase().toCharArray();
+            char[] b=s2.toLowerCase().toCharArray();
+            Arrays.sort(a);
+            Arrays.sort(b);
+            if(Arrays.equals(a,b)){
+                System.out.println("he given strings are Anagrams.");
+            }
+            else{
+                System.out.println("the given strings are not Anagrams.");
+            }
+        }
+        catch(Exception e){
+            System.out.println("Error: "+e.getMessage());
+        }
+        finally{
+            sc.close();
+        }
     }
-    
 }
